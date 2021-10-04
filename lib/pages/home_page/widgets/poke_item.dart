bac54f11821edfb1638eeccf12e865440f2c1fea
Leonardo Blanski <leonardo.blanski@giver.com.br>
2021-10-04 13:06:41 -0300

    Implementações API

---
+++ lib/pages/home_page/widgets/poke_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class PokeItem extends StatelessWidget {
+
+  final String name;
+  final int index;
+  final Color color;
+  final Widget image;
+
+  const PokeItem({Key key, this.name, this.index, this.color, this.image}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: <Widget>[
+            image,
+          ],
+        ),
+        decoration: BoxDecoration(
+          
+          color: Colors.grey, 
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),          
+          ),
+        ),        
+      ),
+    );
+  }
+}
